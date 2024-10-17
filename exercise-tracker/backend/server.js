74604abbee150f4608b72a370312db5759b743b5
Arnav D <arnav.deepaware@gmail.com>
2024-10-17 19:17:58 -0400

    Added new backend connection

---
+++ exercise-tracker/backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const  port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+//MongoDB URI
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+//Connection to database
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB Database connection established successfully');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
